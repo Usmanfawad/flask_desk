1ab9e9be6f0dc5aa4b6acfbc5f649ad5585160a5
Usman Fawad <45047731+Usmanfawad@users.noreply.github.com>
2020-05-16 13:59:46 +0500

    Update run.py
    Disabled debug mode

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ from ProDesk import app
 # from ProDesk.models import *
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
     # app.debug=True
     # app.run(host="0.0.0.0")
